46b1d7ff965280290c57e9fa335231d07846ec67
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-07-23 18:16:57 -0300

    feat: added base shadcn components

--- apps/byte-bazar/components/providers.tsx
+++ apps/byte-bazar/components/providers.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { Toaster } from "@workspace/ui/components/sonner";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import * as React from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -12,7 +13,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
       enableColorScheme
     >
+      <Toaster />
       {children}
     </NextThemesProvider>
-  )
+  );
 }
