105e18a0ec16966fba4625e6d3a9b0135959c8df
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-08-05 01:16:21 -0300

    fix: extendend product schema zod

--- apps/byte-bazar/lib/schemas/products/products.schema.ts
+++ apps/byte-bazar/lib/schemas/products/products.schema.ts
@@ -3,15 +3,38 @@ import zod from "zod";
 const productsSchema = zod.object({
   name: zod.string().min(1, { message: "Product name is required" }),
   description: zod.string().min(1, { message: "Description is required" }),
+  sku: zod.string().min(1, { message: "SKU is required" }),
   price: zod.number().positive({ message: "Price must be a positive number" }),
-  category: zod.string().min(1, { message: "Category is required" }),
-  brand: zod.string().min(1, { message: "Brand is required" }),
-  imageUrl: zod.string().url({ message: "Image URL must be a valid URL" }),
+  comparePrice: zod.number(),
+  stock: zod.number().int().nonnegative({
+    message: "Stock must be a non-negative integer",
+  }),
+  minStock: zod.number().int().nonnegative({
+    message: "Minimum stock must be a non-negative integer",
+  }),
 
-  stock: zod
-    .number()
-    .int({ message: "Stock must be an integer" })
-    .nonnegative({ message: "Stock cannot be negative" }),
+  categoryId: zod.string().min(1, { message: "Category is required" }),
+  brandId: zod.string().min(1, { message: "Brand is required" }),
+  imageUrl: zod.string().url({ message: "Image URL must be a valid URL" }),
+  images: zod.array(zod.string().url({ message: "Image URL must be valid" })),
+  tags: zod.array(zod.string().min(1, { message: "Tag cannot be empty" })),
+
+  discountPercentage: zod.number(),
+  discountStartDate: zod.date(),
+  discountEndDate: zod.date(),
+
+  isActive: zod.boolean(),
+  isFeatured: zod.boolean(),
+  Weight: zod.number(),
+  dimensions: zod.object({
+    length: zod.number(),
+    width: zod.number(),
+    height: zod.number(),
+  }),
+  slug: zod.string(),
+  Views: zod.number().default(0),
+  metaTitle: zod.string(),
+  metaDescription: zod.string(),
 });
 export default productsSchema;
 
