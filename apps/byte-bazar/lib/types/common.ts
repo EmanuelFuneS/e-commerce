5883d3870351dd14cfde86e662358e26837afa15
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-08-05 01:14:28 -0300

    feat: added common types

---
+++ apps/byte-bazar/lib/types/common.ts
@@ -0,0 +1,18 @@
+export interface ApiResponse<T = any> {
+  success: boolean;
+  data?: T;
+  error?: string;
+  message?: string;
+  details?: any;
+  total?: number;
+  pagination?: Pagination;
+}
+
+interface Pagination {
+  page: number;
+  pageSize: number;
+  totalPages: number;
+  totalItems: number;
+}
+
+export interface ActionResponse extends ApiResponse {}
