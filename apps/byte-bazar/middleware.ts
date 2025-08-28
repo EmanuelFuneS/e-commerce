4d7ee1966eb077c130fad46d76232f23f2a63681
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-08-28 02:12:25 +0000

    test: test auth0 protection routes

--- apps/byte-bazar/middleware.ts
+++ apps/byte-bazar/middleware.ts
@@ -1,12 +1,25 @@
 import type { NextRequest } from "next/server";
 import { auth0 } from "./lib/auth0";
 
+const protectedRoutes = ["/dashboard"];
+
 export async function middleware(request: NextRequest) {
-  return await auth0.middleware(request);
+  const { pathname } = request.nextUrl;
+
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (isProtectedRoute) {
+    return await auth0.middleware(request);
+  }
+
+  return;
 }
 
 export const config = {
   matcher: [
     "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/dashboard/:path*",
   ],
 };
