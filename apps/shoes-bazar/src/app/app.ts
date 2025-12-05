ec492a6bbda9de9d80cfeb679130d694aa9686d3
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-12-05 18:11:56 +0000

    feat: config and added img component

--- apps/shoes-bazar/src/app/app.ts
+++ apps/shoes-bazar/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Img } from './components/img/img';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Img],
   templateUrl: './app.html',
-  styleUrl: './app.less'
+  styleUrl: './app.less',
 })
 export class App {
   protected readonly title = signal('shoes-bazar');
