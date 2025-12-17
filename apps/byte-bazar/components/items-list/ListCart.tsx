30dafb40b7d3211fa6bb61534a57fb3a9fce7f5d
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-12-17 22:31:41 +0000

    feat: added component

---
+++ apps/byte-bazar/components/items-list/ListCart.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../../../packages/ui/src/components";
+
+type Props = {};
+
+const ListCart = (props: Props) => {
+  const cartItems = new Array(4).fill("");
+  return (
+    <Card className="">
+      <CardHeader className="flex flex-row justify-between">
+        <CardTitle>Product </CardTitle>
+        <CardTitle>Price </CardTitle>
+        <CardTitle>Quantity</CardTitle>
+      </CardHeader>
+      <CardContent className="mt-10 space-y-10 space-x-5">
+        {cartItems.map((item, idx) => {
+          return (
+            <div className="w-full h-10 flex justify-between border-b-2 pb-10 mt-4 gap-6">
+              <p>name product</p>
+              <p>999</p>
+              <p className="mx-7">12</p>
+            </div>
+          );
+        })}
+      </CardContent>
+      <CardFooter className="mt-10 flex justify-between">
+        <Button>
+          <Link href={"/products"}>Return To Shop</Link>
+        </Button>
+        <Button>
+          <Link href={""}>Update Cart</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ListCart;
