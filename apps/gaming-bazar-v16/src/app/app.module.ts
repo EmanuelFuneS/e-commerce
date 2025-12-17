c69fbee0b3eba7860f5a624de2e5ed867afaae6e
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-12-17 23:38:01 +0000

    fix: fix for render games in components

--- apps/gaming-bazar-v16/src/app/app.module.ts
+++ apps/gaming-bazar-v16/src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameProductComponent } from './components/game-product/game-product.component';
-import { ImgComponent } from './components/img/img.component';
-import { GameProductsComponent } from './components/game-products/game-products.component';
-import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { GameProductComponent } from './components/game-product/game-product.component';
+import { GameProductsComponent } from './components/game-products/game-products.component';
+import { ImgComponent } from './components/img/img.component';
+import { NavComponent } from './components/nav/nav.component';
+import { VideogamesService } from './services/videogames.service';
 
 @NgModule({
-  declarations: [AppComponent, ImgComponent, GameProductComponent, GameProductsComponent, NavComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    ImgComponent,
+    GameProductComponent,
+    GameProductsComponent,
+    NavComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [VideogamesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
