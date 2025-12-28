dae45f19ea7dcc6c3014163b4b42ccec4836c8e0
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-12-28 00:12:51 +0000

    feat: added database package, provide prisma service in user service, added method create and register

--- services/auth-service/src/users/users.service.ts
+++ services/auth-service/src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 export type User = {
   userId: number;
@@ -8,6 +9,8 @@ export type User = {
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   private readonly users = [
     {
       userId: 1,
@@ -24,5 +27,11 @@ export class UsersService {
   // eslint-disable-next-line @typescript-eslint/require-await
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
+  }
+
+  async create(user) {
+    return await this.prisma.client.user.create({
+      data: user,
+    });
   }
 }
