a5d05955764442a4ed2f8b8c6bf572e79c7acfda
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-12-30 19:42:31 +0000

    feat: extend routes

--- apps/gaming-bazar-v16/src/app/app-routing.module.ts
+++ apps/gaming-bazar-v16/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GameDetailComponent } from './pages/game-detail/game-detail.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: LandingComponent,
+  },
+  {
+    path: 'about',
+    component: LandingComponent,
+  },
+  {
+    path: 'detail/:id',
+    component: GameDetailComponent,
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
