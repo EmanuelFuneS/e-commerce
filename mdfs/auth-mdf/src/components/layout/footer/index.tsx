a538460751651082575fca9a7aef2770a7f87e58
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-01-15 21:26:29 +0000

    fix: style

--- mdfs/auth-mdf/src/components/layout/footer/index.tsx
+++ mdfs/auth-mdf/src/components/layout/footer/index.tsx
@@ -4,8 +4,10 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer className=" w-full h-10 flex justify-center items-center bg-card">
-      <Label>@2025 AuthService Inc. All rights reserved</Label>
+    <footer className="w-full h-10 flex justify-center items-center bg-card">
+      <Label className="text-xs">
+        @2025 AuthService Inc. All rights reserved
+      </Label>
     </footer>
   );
 };
--- mdfs/auth-mdf/src/components/layout/nav-bar/index.tsx
+++ mdfs/auth-mdf/src/components/layout/nav-bar/index.tsx
@@ -7,8 +7,8 @@ const Navbar = (props: Props) => {
   return (
     <header className="w-full bg-card border-b-2 h-10 flex justify-between px-5">
       <Label>AuthService</Label>
-      <Label>
-        Don't have an account?
+      <Label className="text-xs ">
+        Don't have an account ?.
         <Link to="/register">Sign Up</Link>
       </Label>
     </header>
