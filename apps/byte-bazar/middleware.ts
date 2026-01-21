083952615abd602a632944d0823edc7dc1ea8df6
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-01-21 15:48:58 +0000

    feat: added middleware

---
+++ apps/byte-bazar/middleware.ts
@@ -0,0 +1,27 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const protectedRoutes = ["/dashboard", "/orders", "checkout", "/settings"];
+
+const authRoutes = ["/login", "/register"];
+
+export const middleware = (request: NextRequest) => {
+  const token = request.cookies.get("token");
+  const { pathname } = request.nextUrl;
+
+  if (protectedRoutes.some((route) => pathname.startsWith(route)) && !token) {
+    const url = new URL("auth/login", request.url);
+    url.searchParams.set("callbackUrl", pathname);
+    return NextResponse.redirect(url);
+  }
+
+  if (authRoutes.some((route) => pathname.startsWith(route)) && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
