68a2fb7c869272eb1e86488bd49a5dc46bf89e51
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-03-27 15:45:40 -0300

    feat: added swagger and implment

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth Api')
+    .setDescription('Auth Documentation')
+    .setVersion('1.0')
+    .addTag('AUTH')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   app.enableCors({
     origin: process.env.CORS_ORIGIN?.split(','),
     credentials: true,
