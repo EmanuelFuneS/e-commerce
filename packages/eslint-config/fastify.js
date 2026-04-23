d448a2baddab7dea9f5c3afbb634186a913bf327
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-04-23 16:03:29 -0300

    fix: minor changes

---
+++ packages/eslint-config/fastify.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
+import onlyWarn from "eslint-plugin-only-warn"
+import turboPlugin from "eslint-plugin-turbo"
+import tseslint from "typescript-eslint"
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config}
+ * */
+export const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-floating-promises": "warn",
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+]
+export { config as fastifyConfig } from "./src/fastify.js"
